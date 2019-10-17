51b1e27ef45007a56e7db2319a775458e565b479
Vitalii Nikolaienko <vitalii.nikolaienko@student.tuke.sk>
2019-10-17 14:34:06 +0200

    update About and Games activities

--- app/src/main/java/com/example/gameconsole/Games.java
+++ app/src/main/java/com/example/gameconsole/Games.java
@@ -15,6 +15,8 @@ public class Games extends AppCompatActivity {
         setContentView(R.layout.activity_games);
         Button maze = this.findViewById(R.id.maze);
         Button coloring = this.findViewById(R.id.coloring);
+        Button snake = this.findViewById(R.id.snake);
+        Button dj = this.findViewById(R.id.doodle_jump);
         maze.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -25,6 +27,18 @@ public class Games extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(Games.this, Coloring.class));
+            }
+        });
+        snake.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Games.this, Snake.class));
+            }
+        });
+        dj.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Games.this, DoodleJump.class));
             }
         });
     }
